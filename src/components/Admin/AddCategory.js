4699085a7097d4e4c38f70cac0d3d69edb9ca30c
unknown <truongbao901@gmail.com>
2022-05-21 21:57:50 +0700

    commit

--- src/components/Admin/AddCategory.js
+++ src/components/Admin/AddCategory.js
@@ -1,11 +1,24 @@
 
 import React, { useState, useEffect } from 'react'
 export function AddCategory({ close }){
+    const url="https://localhost:44380/api/Category/AddCate?Name=";
+    const [data,setData]=useState({
+        Name:""
+    })
+    function submit(e){
+        e.preventDefault();
+        var t= document.getElementById('text-tickets').value
+        console.log(t)
+        fetch(url+t,{
+            method:'POST',
+        })
+        .then(window.location.href=window.location.href)
+    }
     return(
         <div className="modalver2" style={{ position: 'fixed', top: '0', bottom: '0', right: '0', left: 0, zIndex: '4' }}>
                     <div className="modal-container">
                         <div className="closever2">
-                        <a className="close" onClick={close}>
+                        <a style={{textDecoration:'none'}} className="close" onClick={close}>
                         &times;
                       </a>
                         </div>
@@ -13,9 +26,8 @@ export function AddCategory({ close }){
                             <i className="far fa-edit"></i>
                             Thêm Danh Mục
                         </header>
-                        <form id="addform">
-                            <div className="modal-body" style={{ maxHeight: '100px' }}>
-
+                        <form id="addform" onSubmit={(e) => submit(e)}>
+                            <div className="modal-body" style={{ maxHeight: '130px' }}>
                                 <div className="modal-category-name">
                                     <label htmlFor="text-tickets" className="modal-label">
                                         <strong>Tên Danh Mục</strong>
@@ -25,7 +37,7 @@ export function AddCategory({ close }){
                             </div>
 
                             <footer className="modal-footer">
-                                <button className="buy-tickets save">
+                                <button type='submit' className="buy-tickets save">
                                     <i className="fas fa-check"></i>
                                     LƯU
                                 </button>
--- src/components/Admin/LayoutAdmin.js
+++ src/components/Admin/LayoutAdmin.js
@@ -36,7 +36,7 @@ export function LayoutAdmin() {
     }
     window.addEventListener("scroll", setFixed)
     return (
-        <div className="wrapper" style={{ overflow: 'hidden' }}>
+        <div className="wrapper" style={{ overflow: 'hidden' , marginTop:'100px'}}>
             <div className={fix ? 'navbar fixed' : 'navbar'} style={{ position: 'fixed' }}>
                 <div className="container">
                     {/*                      <div className="logo"><img height="100" style={style} src={Logo}/></div>*/}
@@ -64,70 +64,82 @@ export function LayoutAdmin() {
 
                 </div>
             </div>
-            <div className="row" style={{ minHeight: '110vh', marginTop: '100px' }}>
-                <div className="col-2" style={{ position: 'fixed', top: '0', bottom: '0', paddingTop: '4%' }}>
-                    <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                        <div className="sb-sidenav-menu">
-                            <div className="nav">
-                                <div className="sb-sidenav-menu-heading" style={{ fontSize: '20px', fontWeight: 'bold', paddingTop: '50px' }}>Danh mục quản lí</div>
-
-                                <a className="nav-link" href="/admin" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon">{/*<i className="fas fa-user"></i>*/}</div>
-                                    Thông tin cá nhân
-                                </a>
-                                <a className="nav-link" href="/admin/changepassword" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon">
-                                        <i className="fas fa-key"></i>
-                                    </div>
-                                    Đổi mật khẩu
-                                </a>
-                                <a className="nav-link" href="/admin/StaffManagement" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon"></div>
-                                    Quản Lý Nhân Viên
-                                </a>
-                                <a className="nav-link" href="/admin/product" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon"></div>
-                                    Sản Phẩm
-                                </a>
-                               
-                                <a className="nav-link" href="/admin/category" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon"></div>
-                                    Danh Mục
-                                </a>
-                                <a className="nav-link" href="/admin/invoicedetail" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon"></div>
-                                    Chi Tiết Đơn Hàng
-                                </a>
-                                <a className="nav-link" href="/admin/chart" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon"></div>
-                                    Bảng Thống Kê
-                                </a>
-                                <a className="nav-link" href="/admin/setting" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon"></div>
-                                    Thiết lập trang web
-                                </a>
-                                <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                    <div className="sb-nav-link-icon"></div>
-                                    Thùng Rác
-                                    <div className="sb-sidenav-collapse-arrow"></div>
-                                </a>
-
-                                <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                    <nav className="sb-sidenav-menu-nested nav">
-                                        <a className="nav-link" href="/admin/bannedstaff">-Nhân Viên</a>
-                                        <a className="nav-link" href="/admin/DeletedAttribute">-Thuộc Tính</a>
-                                        <a className="nav-link" href="/admin/DeletedCategory">-Danh Mục</a>
-                                        <a className="nav-link" href="/admin/deletedproduct">-Sản Phẩm</a>
-                                    </nav>
-                                </div>
-                                <a className="nav-link" href="/adminauth/LogOut" style={{ fontSize: '18px' }}>
-                                    <div className="sb-nav-link-icon"><i className="fas fa-sign-out-alt"></i></div>
-                                    Đăng xuất
-                                </a>
+            <div>
+            <div className="container-fluid page-body-wrapper" style={{display:'inline'}}>
+                <nav className="sidebar sidebar-offcanvas" id="sidebar" style={{ zIndex: '0', marginTop: '-10px',width:'250px' ,position:'fixed'}}>
+                    <ul className="nav">
+                        <li className="nav-item nav-category">Quản lí</li>
+                        <li className="nav-item">
+                            <a className="nav-link" data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="form-elements">
+                                <i className="menu-icon mdi mdi-card-text-outline"></i>
+                                <span className="menu-title">Sản Phẩm</span>
+                                {/* <i className="menu-arrow"></i> */}
+                            </a>
+                            <div className="collapse" id="form-elements">
+                                <ul className="nav flex-column sub-menu">
+                                    <li className="nav-item"><a className="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
+                                </ul>
                             </div>
-                        </div>
-                    </nav>
-                </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="charts">
+                                <i className="menu-icon mdi mdi-chart-line"></i>
+                                <span className="menu-title">Danh mục</span>
+                                {/* <i className="menu-arrow"></i> */}
+                            </a>
+                            {/* <div className="collapse" id="charts">
+                                <ul className="nav flex-column sub-menu">
+                                    <li className="nav-item"> <a className="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
+                                </ul>
+                            </div> */}
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="tables">
+                                <i className="menu-icon mdi mdi-table"></i>
+                                <span className="menu-title">Thuộc tính</span>
+                                {/* <i className="menu-arrow"></i> */}
+                            </a>
+                            {/* <div className="collapse" id="tables">
+                                <ul className="nav flex-column sub-menu">
+                                    <li className="nav-item"> <a className="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
+                                </ul>
+                            </div> */}
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="icons">
+                                <i className="menu-icon mdi mdi-layers-outline"></i>
+                                <span className="menu-title">Icons</span>
+                                {/* <i className="menu-arrow"></i> */}
+                            </a>
+                            {/* <div className="collapse" id="icons">
+                                <ul className="nav flex-column sub-menu">
+                                    <li className="nav-item"> <a className="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
+                                </ul>
+                            </div> */}
+                        </li>
+                        <li className="nav-item nav-category">Thống kê</li>
+                        <li className="nav-item">
+                            <a className="nav-link" data-bs-toggle="collapse" href="#" aria-expanded="false" aria-controls="auth">
+                                <i className="menu-icon mdi mdi-account-circle-outline"></i>
+                                <span className="menu-title">Biểu đồ</span>
+                                {/* <i className="menu-arrow"></i> */}
+                            </a>
+                            {/* <div className="collapse" id="auth">
+                                <ul className="nav flex-column sub-menu">
+                                    <li className="nav-item"> <a className="nav-link" href="pages/samples/login.html"> Login </a></li>
+                                </ul>
+                            </div> */}
+                        </li>
+                        <li className="nav-item nav-category">tác vụ</li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="http://bootstrapdash.com/demo/star-admin2-free/docs/documentation.html">
+                                <i className="menu-icon mdi mdi-file-document"></i>
+                                <span className="menu-title">Đăng Xuất</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
                 <div style={{ paddingLeft: '18%' }}>
                     <Routes>
                         <Route path="/" element={<Navigate replace to="/admin/home" />} />
