94a2f452eb57b97c08b2f75d7113e03fb967bf0b
InoriZ <quocthai.jy9a13@gmail.com>
2022-06-14 20:50:56 +0700

    Xóa Product

--- src/components/Admin/Category.js
+++ src/components/Admin/Category.js
@@ -9,10 +9,7 @@ import swal from 'sweetalert'
 // import $ from 'jquery';
 export function Category() {
     const url = `https://localhost:44380/api/Category`
-    const delUrl="https://localhost:44380/api/Category/DeleteCate";
-
-
-
+    const delUrl="https://localhost:44380/api/Category/DeleteCate"
     function submit(item,e){
         var id=item.idCategory;
         console.log(id);
