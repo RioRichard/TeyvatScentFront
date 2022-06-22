a22de0146e1fbe37652e62ba60e067ed3ba8d87c
InoriZ <quocthai.jy9a13@gmail.com>
2022-06-22 09:40:26 +0700

    Lmao

--- src/components/Admin/AdminLogging.js
+++ src/components/Admin/AdminLogging.js
@@ -18,7 +18,7 @@ export function AdminLogging()
                 {
                     "userName": user,
                     "pass": pass,
-                    "urlFrontEnd" : ""
+                    "urlFrontEnd" : "/"
                 }
             )
         })
